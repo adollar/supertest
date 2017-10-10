a2e9bea6f58a70d614c2f414e0f802c81a034e96
Vitaliy <vkilbasovich@gmail.com>
2017-10-10 15:02:54 +0300

    Add files via upload

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,7 +14,12 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $numberl= mt_rand(1, 100);
-        return $this->render('kilbas/kilbas.html.twig', array('luckynumb' => $numberl,'colorr'=>null));
+        $url = 'http://api.openweathermap.org/data/2.5/weather?id=698740&units=metric&APPID=c4d64189685c30187df7546364d23e5b';
+        $obj = json_decode(file_get_contents($url), true);
+        return $this->render('kilbas/kilbas.html.twig', [
+            'wind_speed' => $obj['wind']['speed'], 'wea_ther'=> $obj['main']['temp'], 'luckynumb' => $numberl,'colorr'=>null]);
+
+        
     }
 
     /**
@@ -23,8 +28,10 @@ class DefaultController extends Controller
     */
     public function pageAction()
     {
+        $url = 'http://api.openweathermap.org/data/2.5/weather?q=Odessa&APPID=c4d64189685c30187df7546364d23e5b';
+        $obj = json_decode(file_get_contents($url), true);
         $num=mt_rand(50,55);
-        return $this->render('kilbas/kilbas.html.twig', array('luckynumb'=>$num, 'colorr'=>null));
+        return $this->render('kilbas/kilbas.html.twig', array('wind_speed' => $obj['wind']['speed'], 'luckynumb'=>$num, 'colorr'=>null));
     }
 
     /**
@@ -33,9 +40,13 @@ class DefaultController extends Controller
     */
     public function zveropolisAction()
     {
+        $url = 'http://api.openweathermap.org/data/2.5/weather?q=Odessa&APPID=c4d64189685c30187df7546364d23e5b';
+        $obj = json_decode(file_get_contents($url), true);
         $colori=mt_rand(100,900);
-        return $this->render('kilbas/kilbas.html.twig', array('colorr'=>$colori, 'luckynumb'=>'нет'));
+        return $this->render('kilbas/kilbas.html.twig', array('wind_speed' => $obj['wind']['speed'], 'colorr'=>$colori, 'luckynumb'=>'нет'));
     }
+
+  
 
 }
 
